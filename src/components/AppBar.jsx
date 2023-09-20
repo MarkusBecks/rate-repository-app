96d38b454ffe3978e7d64d0396576a24af3474d1
Markus <markus.becks@gmail.com>
2023-09-20 14:15:47 +0300

    expo sdk 49, add SignIn, style AppBar

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,26 +1,39 @@
-import { View, StyleSheet } from 'react-native'
+import { View, StyleSheet, ScrollView } from 'react-native'
 import Constants from 'expo-constants'
 import theme from '../theme'
 import AppBarTab from './AppBarTab'
 
 const styles = StyleSheet.create({
-  flexContainer: {
+  appBarContainer: {
     paddingTop: Constants.statusBarHeight * 1.5,
     paddingBottom: Constants.statusBarHeight / 2,
-    paddingHorizontal: Constants.statusBarHeight / 2,
     backgroundColor: theme.colors.appBarBg,
-    opacity: 0.95,
+    display: 'flex',
+  },
+  scrollViewContent: {
     display: 'flex',
     flexDirection: 'row',
+    paddingHorizontal: 15,
     justifyContent: 'space-around',
+    flexGrow: 1,
   },
 })
 
 const AppBar = () => {
   return (
-    <View style={styles.flexContainer}>
-      <AppBarTab title="Repositories" />
-      <AppBarTab title="Secondary tab" />
+    <View style={styles.appBarContainer}>
+      <ScrollView
+        contentContainerStyle={styles.scrollViewContent}
+        horizontal={true}
+      >
+        <AppBarTab title="Repositories" to="/" />
+        <AppBarTab title="Sign In" to="/signin" />
+        <AppBarTab title="Repositories" to="/" />
+        <AppBarTab title="Sign In" to="/signin" />
+        <AppBarTab title="Sign In" to="/signin" />
+        <AppBarTab title="Sign In" to="/signin" />
+        <AppBarTab title="Sign In" to="/signin" />
+      </ScrollView>
     </View>
   )
 }
--- src/components/AppBarTab.jsx
+++ src/components/AppBarTab.jsx
@@ -1,17 +1,23 @@
 import { View, StyleSheet, Pressable } from 'react-native'
+import { Link } from 'react-router-native'
 import Text from './Text'
 
 const styles = StyleSheet.create({
-  flexTab: {},
+  tab: {
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
 })
 
-const AppBarTab = ({ title }) => {
+const AppBarTab = ({ title, to }) => {
   return (
-    <View style={styles.flexTab}>
+    <View style={styles.tab}>
       <Pressable>
-        <Text fontWeight="bold" color="white">
-          {title}
-        </Text>
+        <Link to={to}>
+          <Text fontWeight="bold" color="white">
+            {title}
+          </Text>
+        </Link>
       </Pressable>
     </View>
   )
